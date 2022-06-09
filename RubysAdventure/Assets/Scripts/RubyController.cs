8c6aa78df4d4a6ef1a594e9a3a22c5b047a1b26c
Shelley Benhoff <shelley@hoffstech.com>
2022-06-08 20:06:16 -0400

    Added world objects and worked with collisions and physics

--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3774971ac32b6a032b17e4daee387f86e06cc0fa526990ca65152f9643459dcf
-size 686
+oid sha256:60164f8f262830fcfa4f8e91784f7a136a7e28042858ed2c82a6ccc247bb439b
+size 1080
